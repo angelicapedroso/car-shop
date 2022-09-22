fc3d0b83a0a17cb6bc5419b0ccc091f44ccaa9fb
angelicapedroso <angelicapedroso05@gmail.com>
2022-09-21 23:19:36 -0300

    test(service): readOne

--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -5,6 +5,7 @@ import CarModel from '../../../models/Car';
 import CarService from '../../../services/Car';
 import * as mock from '../../mocks/carMock';
 import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
 
 describe('Car Service', () => {
   const carModel = new CarModel();
@@ -13,13 +14,17 @@ describe('Car Service', () => {
   before(async () => {
     sinon.stub(carModel, 'create').resolves(mock.carMockWithId);
     sinon.stub(carModel, 'read').resolves(mock.allCarsMock);
+    sinon.stub(carModel, 'readOne')
+      .onCall(0).resolves(mock.carMockWithId)
+      .onCall(1).resolves(null)
+      .onCall(2).resolves(mock.carMockWithId)
   });
 
-  after(()=>{
+  after(() => {
     sinon.restore();
   })
 
-  describe('Create car', () => {
+  describe('Create', () => {
     it('success', async () => {
       const newCar = await carService.create(mock.carMock);
       expect(newCar).to.be.deep.equal(mock.carMockWithId);
@@ -34,11 +39,27 @@ describe('Car Service', () => {
     })
   })
 
-  describe('list cars', () => {
+  describe('Read', () => {
     it('success', async () => {
       const cars = await carService.read();
       expect(cars).to.be.deep.equal(mock.allCarsMock);
     })
   })
+
+  describe('ReadOne', () => {
+		it('Success', async () => {
+			const car = await carService.readOne(mock.carMockWithId._id);
+
+			expect(car).to.be.deep.equal(mock.carMockWithId);
+		});
+
+		it('Failure', async () => {
+			try {
+				await carService.readOne(mock.carMockWithId._id);
+			} catch (error:any) {
+				expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+			}
+		});
+	});
 
 });
